0e629cdd9abcbfd584f6479262c6c3e63557d821
HeavenlySea <pavelking11@mail.ru>
2021-12-18 12:03:05 +0300

    fix path to image

--- changeWindowBackground/changeBackground.py
+++ changeWindowBackground/changeBackground.py
@@ -20,24 +20,24 @@ class ChangeBackground():
                 raise FileNotFoundError(f"В папке {self.get_absolute_path()} не найдены обои")
             return list_background
         else:
-            list_background = sum([file_name for root, dirs, file_name in self.path], [])
+            list_background = sum([file_name for root, dirs, file_name in walk(self.path)], [])
             if not list_background:
                 raise FileNotFoundError(f"В папке {self.path} не найдены обои")
             return list_background
 
     def change_random_background(self):
         list_window_background = self.get_picture_for_window_background()
+        path = self.get_absolute_path()
         if not self.path:
-            path = self.get_absolute_path()
             if len(list_window_background) > 1:
                 ctypes.windll.user32.SystemParametersInfoW(self.SPI_SETDESKWALLPAPER, 0, r"{0}\{1}".format(path, list_window_background[randint(0, len(list_window_background) - 1)]), 0)
             elif len(list_window_background) == 1:
                 ctypes.windll.user32.SystemParametersInfoW(self.SPI_SETDESKWALLPAPER, 0, r"{0}\{1}".format(path, list_window_background[0]), 0)
         else:
             if len(list_window_background) > 1:
-                ctypes.windll.user32.SystemParametersInfoW(self.SPI_SETDESKWALLPAPER, 0, r"{0}\{1}".format(path, list_window_background[randint(0, len(list_window_background) - 1)]), 0)
+                ctypes.windll.user32.SystemParametersInfoW(self.SPI_SETDESKWALLPAPER, 0, r"{0}\{1}".format(self.path, list_window_background[randint(0, len(list_window_background) - 1)]), 0)
             elif len(list_window_backround) == 1:
-                ctypes.windll.user32.SystemParametersInfoW(self.SPI_SETDESKWALLPAPER, 0, r"{0}\{1}".format(path, list_window_background[0]), 0)
+                ctypes.windll.user32.SystemParametersInfoW(self.SPI_SETDESKWALLPAPER, 0, r"{0}\{1}".format(self.path, list_window_background[0]), 0)
 
     def change_backgorund(self):
         if self.path:
